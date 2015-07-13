442e4f54645366b5e1b2b70227bf82e7c08a4cfc
Andrew Ross <andrew@vermonster.com>
2015-07-13 13:24:37 -0400

    pairing is caring

--- chorebot.rb
+++ chorebot.rb
@@ -12,18 +12,25 @@ def post_message(message)
   HTTParty.post(ENV['SLACK_WEBHOOK_URL'], body: params.to_json)
 end
 
-def current_trash_assignee
-  candidates = member_names
-  weekday_of_year = Date.today.cweek*5 + Date.today.wday
-  candidates[weekday_of_year % candidates.length]
+def assignees_for(day, candidates)
+  i1 = 2*(day.cweek*5 + day.wday) + 5
+  i2 = i1 + 1
+  member1 = candidates[i1 % candidates.length]
+  member2 = candidates[i2 % candidates.length]
+  [member1, member2]
+end
+
+def current_trash_assignees
+  member1, member2 = assignees_for(Date.today, member_names)
+  "<@#{member1}> and <@#{member2}>"
 end
 
 def morning_trash_message
-  post_message("Good morning <@#{current_trash_assignee}>! It's your turn to take out the trash!")
+  post_message("Good morning #{current_trash_assignees}! It's your turn to take out the trash!")
 end
 
 def afternoon_trash_message
-  post_message("Good afternoon <@#{current_trash_assignee}>! Did you remember to take out the trash?")
+  post_message("Good afternoon #{current_trash_assignees}! Did you remember to take out the trash?")
 end
 
 def weekly_cleanup_message
